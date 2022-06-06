d58972725bcf976c1c6f877b4095e0cd15b87e10
hmurata <murata@sorcerer.co.jp>
2022-06-06 11:41:59 +0900

    vue3のサンプル

---
+++ vue3/src/components/sampleComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>{{text}}</h2>
+    <p>{{name}}</p>
+    <p>{{count}}</p>
+    <button @click="add">add</button>
+    <button @click="sub">sub</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data: () => {
+      return {
+        text: '',
+        count: 0
+      }
+    },
+    props: ['name'],
+    created() {
+      this.text = "create update";
+    },
+    methods: {
+      add(){
+        this.count += 1;
+      },
+      sub(){
+        this.count -= 1;
+      }
+    },
+    computed: {
+    },
+  }
+</script>
---
+++ vue3/src/views/mainView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ text }}</h1>
+    <button @click="textChange">text change</button>
+    <sample-component name="prop_name"></sample-component>
+  </div>
+</template>
+
+<script>
+import sampleComponent from "@/components/sampleComponent";
+export default {
+  name: 'mainView',
+  components: {
+    sampleComponent
+  },
+  data: function (){
+    return {
+      text: "hello world",
+      prop_update: "prop_update",
+      prop_name: ' prop_name'
+    }
+  },
+  // created() {
+  // },
+  // mounted: function (){
+  // },
+  methods: {
+    textChange(){
+      this.text = "good new world";
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
