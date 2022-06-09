e36e20109a8a90d69b8636a11723b713346f1690
DESKTOP-J2LPOE1\wym <wym@sorcerer.co.jp>
2022-06-09 14:36:27 +0900

    delete DBHelper DB class

--- src/helpers/DBHelper.ts
+++ src/helpers/DBHelper.ts
@@ -16,29 +16,4 @@ const db_pool = mysql
   })
   .promise();
 
-class DB {
-  private static instance: DB;
-  private static db_pool: mysql.Pool;
-  private constructor() {
-    DB.db_pool = mysql.createPool({
-      host: config.host,
-      user: config.user,
-      database: config.database,
-      password: config.password,
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0,
-    });
-  }
-  static getInstance() {
-    if (!DB.instance) {
-      DB.instance = new DB();
-    }
-    return DB.instance;
-  }
-  pool() {
-    return DB.db_pool.promise();
-  }
-}
-
 export { db_pool };
