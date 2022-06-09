4c0621bd2a4466538f70eb7987e015cbb3244733
DESKTOP-J2LPOE1\wym <wym@sorcerer.co.jp>
2022-06-09 14:31:19 +0900

    DBHelper update

--- src/helpers/DBHelper.ts
+++ src/helpers/DBHelper.ts
@@ -4,22 +4,17 @@ import fs from "fs";
 const path = process.cwd() + "/config/mysql.json";
 const config = JSON.parse(fs.readFileSync(path, "utf8"));
 
-const db_connection = mysql.createConnection({
-  host: config.host,
-  user: config.user,
-  database: config.database,
-  password: config.password,
-}); //同期
-
-const db_pool = mysql.createPool({
-  host: config.host,
-  user: config.user,
-  database: config.database,
-  password: config.password,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const db_pool = mysql
+  .createPool({
+    host: config.host,
+    user: config.user,
+    database: config.database,
+    password: config.password,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  })
+  .promise();
 
 class DB {
   private static instance: DB;
@@ -42,12 +37,8 @@ class DB {
     return DB.instance;
   }
   pool() {
-    return DB.db_pool;
-  }
-
-  pool_promise() {
     return DB.db_pool.promise();
   }
 }
 
-export { db_connection, db_pool };
+export { db_pool };
