84db95b2a3b1ee03d163026a5556d2841e144df7
tomoya okamoto <okamoto@sorcerer.co.jp>
2023-03-09 12:07:17 +0900

    feat: 乱数ヘルパーのテストを追加

--- src/helpers/randomHelper.ts
+++ src/helpers/randomHelper.ts
@@ -4,20 +4,31 @@ const mt = require('mersenne-twister');
 const DEFAULT_MAX = 100000;
 
 interface randomOpt {
+  /** シード */
   seed?: number,
+  /** 生成する数 */
   count?: number,
+  /** 乱数の最大値。 0 < num < max の範囲で生成する。デフォルトは100000。 */
   max?: number
 }
 
-export function generateRandomNumbers(opt: randomOpt): number[] {
+/**
+ * シード対応の乱数の配列を生成する
+ */
+export function generateRandomNumbers(opt?: randomOpt): number[] {
   // デフォルト値を設定
-  const {seed = Date.now(), count = 1, max = DEFAULT_MAX} = opt;
+  const {seed = Date.now(), count = 1, max = DEFAULT_MAX} = opt || {};
+  if (max < 0) {
+    throw new Error("max number is invalid")
+  }
   const mtGen = new mt(seed);
   const results: number[] = [];
   for (let i = 0; i < count; i++) {
     results.push(mtGen.random_int() % max);
   }
+
+  if (results.length != count || results.length < 1) {
+    throw new Error("generated number is invalid")
+  }
   return results;
 }
-
-// module.exports = generateRandomNumbers;
--- tests/helpers/rondomHelper.test.ts
+++ tests/helpers/rondomHelper.test.ts
@@ -1,7 +1,9 @@
 
 import {generateRandomNumbers} from "../../src/helpers/randomHelper";
+import exp = require("constants");
 
 test("seedを指定すれば毎回同じ乱数が発生する", () => {
+  expect.assertions(1);
   const seed = 1;
   const count = 10;
   const result = generateRandomNumbers({seed, count});
@@ -10,9 +12,26 @@ test("seedを指定すれば毎回同じ乱数が発生する", () => {
 })
 
 test("countがマイナス値のとき", () => {
-  const count = -1;
-  const result = generateRandomNumbers({count});
-  // TODO 例外が発生する
-  expect(result).toEqual([]);
+  expect.assertions(1);
+  try {
+    generateRandomNumbers({count: -1});
+  } catch (e) {
+    expect(e).toBeDefined();
+  }
+})
+
+test("maxがマイナス値のとき", () => {
+  expect.assertions(1);
+  try {
+    generateRandomNumbers({max: -1});
+  } catch (e) {
+    expect(e).toBeDefined();
+  }
+})
+test("引数が空のとき", () => {
+  expect.assertions(2);
+  const results = generateRandomNumbers();
+  expect(results).toHaveLength(1);
+  expect(results[0] >= 0).toBeTruthy();
 })
 
